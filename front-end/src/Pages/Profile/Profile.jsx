76bba7a0b6c32da2d62acc340799c1794d42f760
Gabriela Rojas <rojasgabriela901@gmail.com>
2022-04-02 13:18:33 -0700

    showing profile component

--- front-end/src/Pages/Profile/Profile.jsx
+++ front-end/src/Pages/Profile/Profile.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar';
 import Sidebar from '../../Components/Sidebar';
+import ProfileComponent from "../../Components/Profile"
 
 export default class Profile extends React.Component {
+
     render(){
         return(
             <React.Fragment>
-                <Navbar></Navbar>
-                <Sidebar></Sidebar>
+                <Navbar />
+                <Sidebar component = {(<ProfileComponent/>)} />
             </React.Fragment>
         );
     }
